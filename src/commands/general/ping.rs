cb1df0b5f09c0a692c8b060969a3042cfe8c57e8
hllstrrr <hllstrrr@gmail.com>
2026-03-18 03:30:14 +0000

    HELL YEAHH NOW WE USE MACRO FOR SENDING A MESSAGE
    I'll add context later so it's just ctx.reply("") for replying a msg

--- src/commands/general/ping.rs
+++ src/commands/general/ping.rs
@@ -1,16 +1,19 @@
 use crate::cmd;
-use waproto::whatsapp as wa;
 
 cmd!(
     Ping,
     name: "ping",
     aliases: ["p"],
     category: "general",
-    execute: |client, _msg, info| {
-        let pong = wa::Message {
-            conversation: Some("Pong!".to_string()),
-            ..Default::default()
-        };
-        client.send_message(info.source.chat.clone(), pong).await?;
+    execute: |client, _msg, info, state| {
+        send_msg!(
+            client, 
+            info, 
+            state,
+            dst: info.source.chat, 
+            text: "Pong!", 
+            reply: true
+        ).await?;
+        
     }
 );
