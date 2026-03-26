ecc499694f948b71eb677127f68799c21dc23642
hllstrrr <hllstrrr@gmail.com>
2026-03-26 17:58:00 +0000

    fix: using build_quote_context_with_info helper to prevent ghost mention on IOS devices, and deleting auto mention ig, cuz I don't need it rn

--- src/macros.rs
+++ src/macros.rs
@@ -1,10 +1,10 @@
 #[macro_export]
 macro_rules! send_msg {
     ($ctx:expr, dst: $dst:expr, text: $text:expr, reply: $is_reply:expr) => {
-        $crate::send_msg!($ctx.client, $ctx.info, $ctx.state, dst: $dst, text: $text, reply: $is_reply)
+        $crate::send_msg!($ctx.client, $ctx.msg, $ctx.info, $ctx.state, dst: $dst, text: $text, reply: $is_reply)
     };
 
-    ($client:expr, $info:expr, $state:expr, dst: $dst:expr, text: $text:expr, reply: $is_reply:expr) => {{
+    ($client:expr, $msg:expr, $info:expr, $state:expr, dst: $dst:expr, text: $text:expr, reply: $is_reply:expr) => {{
         let mut context = waproto::whatsapp::ContextInfo::default();
         let expiration = $state.get_expiration(&$dst.to_string());
         if expiration > 0 {
@@ -12,18 +12,13 @@ macro_rules! send_msg {
         }
 
         if $is_reply {
-            context.stanza_id = Some($info.id.clone());
-            context.participant = Some($info.source.sender.to_string());
-        }
-
-        let mentions: Vec<String> = $text
-            .split_whitespace()
-            .filter(|word| word.starts_with('@') && word.len() > 1)
-            .map(|m| format!("{}@s.whatsapp.net", &m[1..]))
-            .collect();
-        
-        if !mentions.is_empty() {
-            context.mentioned_jid = mentions;
+            let original_msg = &$msg;
+            context = wacore::proto_helpers::build_quote_context_with_info(
+                $info.id.clone(),
+                &$info.source.sender,
+                &$info.source.chat,
+                &original_msg
+            );
         }
 
         let message = waproto::whatsapp::Message {
