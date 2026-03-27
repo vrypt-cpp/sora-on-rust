9f5d9391e2a2c70af44cf2ed7109d8e26b6de8e4
hllstrrr <hllstrrr@gmail.com>
2026-03-27 14:59:46 +0000

    fix: ghost mention on IOS Devices
    man the documentation pmo so bad, the docs said build_context_with_info is automatically add remote jid to the message but when I saw the function code on docs.rs it's NOT DOING THAT, I can't believe I fumbled up to this stupid shit

--- src/macros.rs
+++ src/macros.rs
@@ -7,20 +7,23 @@ macro_rules! send_msg {
     ($client:expr, $msg:expr, $info:expr, $state:expr, dst: $dst:expr, text: $text:expr, reply: $is_reply:expr) => {{
         let mut context = waproto::whatsapp::ContextInfo::default();
         let expiration = $state.get_expiration(&$dst.to_string());
-        if expiration > 0 {
-            context.expiration = Some(expiration);
-        }
 
         if $is_reply {
             let original_msg = &$msg;
             context = wacore::proto_helpers::build_quote_context_with_info(
                 $info.id.clone(),
-                &$info.source.sender,
+                &$info.source.sender.to_non_ad(),
                 &$info.source.chat,
                 &original_msg
             );
         }
-
+        context.remote_jid = Some($info.source.chat.to_string());
+        context.mentioned_jid = vec![$info.source.sender.to_non_ad().to_string()];
+        if expiration > 0 {
+            context.expiration = Some(expiration);
+        }
+       // println!("{:#?}", &context);
+        
         let message = waproto::whatsapp::Message {
             extended_text_message: Some(Box::new(waproto::whatsapp::message::ExtendedTextMessage {
                 text: Some($text.to_string()),
@@ -30,6 +33,16 @@ macro_rules! send_msg {
             ..Default::default()
         };
 
+
+        // let debug_message = waproto::whatsapp::Message {
+        //     extended_text_message: Some(Box::new(waproto::whatsapp::message::ExtendedTextMessage {
+        //         text: Some(format!("{:#?}", message)),
+        //         ..Default::default()
+        //     })),
+        //     ..Default::default()
+        // };
+        
         $client.send_message($dst.clone(), message)
+        // $client.send_message($dst.clone(), debug_message)
     }};
 }
